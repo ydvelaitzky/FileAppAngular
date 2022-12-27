6547371c960ee8970c7e2585f99c05d05b00f6a6
יוכבד דבליצקי <yocheved_d@malam-payroll.com>
2022-12-27 22:11:03 +0200

    commit

--- src/app/services/file.service.ts
+++ src/app/services/file.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { firstValueFrom, observable, Observable, of } from 'rxjs';
 import { file, fileType } from '../entities/file';
-
+    
 @Injectable({
   providedIn: 'root'
 })
